8db08a7f6aacd3988f347077d84c53d9dd6c1d92
Alex Faktor <118138704+AlexFaktor@users.noreply.github.com>
2023-11-10 22:15:37 +0200

    Cleaned XML-documentation

--- Anagram/Anagram/LineConverter.cs
+++ Anagram/Anagram/LineConverter.cs
@@ -16,9 +16,6 @@ namespace Anagram
         /// <summary>
         /// Works like AnagramWord but separately for each word
         /// </summary>
-        /// <param name="input"></param>
-        /// <returns>string</returns>
-        /// <exception cref="ArgumentNullException"></exception>
         public static string Anagram(string input)
         {  
             if (input == null)
@@ -47,9 +44,6 @@ namespace Anagram
         /// <summary>
         /// Reverses the string
         /// </summary>
-        /// <param name="input"></param>
-        /// <returns></returns>
-        /// <exception cref="ArgumentNullException"></exception>
         private static string Reverse(string input)
         {
             if (input == null)
@@ -70,9 +64,6 @@ namespace Anagram
         /// <summary>
         /// Extracts all non-alphabetic symbols from a string
         /// </summary>
-        /// <param name="input"></param>
-        /// <returns></returns>
-        /// <exception cref="ArgumentNullException"></exception>
         private static string GetNotAlphabeticSymbolsFromString(string input)
         {
             if (input == null)
@@ -94,9 +85,6 @@ namespace Anagram
         /// <summary>
         /// Extracts all alphabetic symbols from a string
         /// </summary>
-        /// <param name="input"></param>
-        /// <returns>string</returns>
-        /// <exception cref="ArgumentNullException"></exception>
         private static string GetAlphabeticSymbolsFromString(string input)
         {
             if (input == null)
@@ -118,9 +106,6 @@ namespace Anagram
         /// <summary>
         /// Returns a string in which non-alphabetic symbols remain in their places, and alphabetic symbols are reversed
         /// </summary>
-        /// <param name="input"></param>
-        /// <returns>string</returns>
-        /// <exception cref="ArgumentNullException"></exception>
         private static string AnagramWord(string input)
         {
             if (input == null)
@@ -157,7 +142,6 @@ namespace Anagram
         /// <summary>
         /// This method uses regular expressions to separate words with spaces
         /// </summary>
-        /// <param name="input"></param>
         /// <returns>an array of strings, where the word is the 1st element, the spaces between the words are the 2nd element...</returns>
         private static string[] SeparateWordsWithSpaces(string input)
         {
